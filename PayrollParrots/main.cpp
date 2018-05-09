2bb81d1fa3b6ea9422bf47a8d4cbfcac5d76171c
mercyharmon <ei7389@wayne.edu>
2018-05-09 12:44:21 -0400

    Initial Commit

---
+++ PayrollParrots/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PayrollParrots
+//
+//  Created by Mercyllia on 5/9/18.
+//  Copyright © 2018 MercylliaHarmon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
